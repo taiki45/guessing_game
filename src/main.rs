824b25ee61750dfda343751ee9a94d0c3ca8f120
Taiki Ono <taiks.4559@gmail.com>
2016-12-29 17:42:37 +0900

    Introduce randomness

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let number = rng.gen_range::<i32>(1, 10);
+
     println!("Guess number!");
 
     println!("Please input your guess:");
-    let mut guess = String::new();
+    let mut input = String::new();
 
-    io::stdin().read_line(&mut guess)
+    io::stdin().read_line(&mut input)
         .expect("Failed to read line");
+    let guess = input.trim();
 
-    println!("You guessed: {}", guess);
+    println!("You guess {}, and secret is {}", guess, number);
+
+    if guess == number.to_string() {
+        println!("Got answer!");
+    } else {
+        println!("You got wrong!");
+    }
 }
